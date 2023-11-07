6bf7de15e1aa3b4396ae06a6087771335464aba0
yukuai <1596848231@qq.com>
2023-11-07 17:39:38 +0800

    更改模型

--- src/model.js
+++ src/model.js
@@ -1665,7 +1665,7 @@ function uvflowing(meshid,direction,block,transfer=0,color){
 BABYLON.SceneLoader.ImportMesh(
     "",
     "model/",
-    "modelv12d.glb",
+    "modelv17d.glb",
     scene,
     function (Meshes) {
         console.log("Meshes:",Meshes)
